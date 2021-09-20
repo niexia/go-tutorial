0c3ee2aa053b8eabc78591f8bdd57da3742fbedb
杨金 <yangjin.im@bytedance.com>
2021-09-20 18:03:49 +0800

    feat: move demo to src

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/niexia/go-tutorial
+
+go 1.17
---
+++ src/demo/ch1/main/hello_world.go
@@ -0,0 +1,14 @@
+// 包，表明代码所在的模块（包）
+package main
+
+// 依赖
+import "fmt"
+// import "os"
+
+// 功能实现
+func main() {
+	fmt.Println("hello world")
+	// if len(os.Args) > 1 {
+	// 	fmt.Println("hello world", os.Args[1])
+	// }
+}
